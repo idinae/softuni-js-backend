f74a37bd964eb41b264807101f8a984ee1e31b9a
idinae <iddinae@gmail.com>
2020-10-23 22:49:26 +0300

    layouts

--- 06.WorkshopMongo/controllers/accessory.js
+++ 06.WorkshopMongo/controllers/accessory.js
@@ -1,18 +1,17 @@
-const cubeModel = require('../models/accessory');
+const accessoryModel = require('../models/accessory');
 module.exports = {
     postCreateAccessory(req, res, next) {
-        const { name, description, imageURL, difficultyLevel } = req.body;
-        cubeModel.create({ name, description, imageURL, difficultyLevel: +difficultyLevel })
+        const { name, description } = req.body;
+        accessoryModel.create({ name, description, imageURL })
         .then(() => res.redirect('/'))
         .catch(next);
     },
     getCreateAccessory(_, res) {
-        res.render('create', { layout: false });
+        res.render('create-accessory', { layout: false });
     },
     postAttachAccessory(req, res, next) {
-        const { name, description, imageURL, difficultyLevel } = req.body;
-        cubeModel.create({ name, description, imageURL, difficultyLevel: +difficultyLevel })
+        const { name, description, imageURL } = req.body;
+        accessoryModel.create({ name, description, imageURL })
         .then(() => res.redirect('/'))
         .catch(next);
     }
-}
--- 06.WorkshopMongo/models/accessory.js
+++ 06.WorkshopMongo/models/accessory.js
@@ -5,8 +5,7 @@ const accessorySchema = new mongoose.Schema({
     name: String,
     description: String,
     imageURL: String,
-    difficultyLevel: Number,
-    cubes:  [Types.ObjectId]
+    cubes: [Types.ObjectId]
 });
 
 module.exports = new mongoose.model('accessory', accessorySchema);
