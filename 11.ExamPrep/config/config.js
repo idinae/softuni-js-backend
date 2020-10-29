395d4f32fb7725f09d249327ab82972fccb73447
idinae <iddinae@gmail.com>
2020-10-29 20:00:59 +0200

    exam skele

--- 11.ExamPrep/config/config.js
+++ 11.ExamPrep/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     development: {
-        port: process.env.PORT || 4000,
+        port: process.env.PORT || 3000,
         privateKey: 'SOFT-UNI-WORKSHOP',
         databaseUrl: `mongodb+srv://user:softuni-password@softuni-5daup.mongodb.net/exam?retryWrites=true&w=majority`
     },
