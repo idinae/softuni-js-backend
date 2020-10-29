25642406f35f8fa9b424a19e4947495f68231fbd
idinae <iddinae@gmail.com>
2020-10-29 20:14:25 +0200

    skele-1 initial

--- 12.Exam/1.Skele/config/index.js
+++ 12.Exam/1.Skele/config/index.js
@@ -3,7 +3,7 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 3000,
-        dbUrl: 'mongodb://localhost:27017/ShoeShelf',
+        dbUrl: 'mongodb://localhost:27017/softuni',
         cookie: 'x-auth-token',
         secret: 'SuperSecretSecret',
         saltRounds: 11
